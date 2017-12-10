7f57bab96f78dbc876ff4d0b05a5797b2b80b718
Evan Wilson <evan@xwing.com>
2017-12-10 12:29:43 -0800

    initial bluetooth connection

---
+++ remote_thermometer.py
@@ -0,0 +1,19 @@
+
+import bluetooth
+import time
+
+BD_ADDR = "B8:27:EB:8B:68:DA"
+PORT = 1
+SAMPLE_FREQ = 1 # Hz
+
+sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+sock.connect((BD_ADDR, PORT))
+
+while (1):
+    try:
+        sock.send("68")
+    except bluetooth.btcommon.BluetoothError:
+        print ("Bluetooth error")
+    time.sleep(60/SAMPLE_FREQ)
+
+sock.close()
