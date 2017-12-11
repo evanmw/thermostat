0555b392066bfd2e1c29e7d387f4d673ea144958
Evan Wilson <evan@xwing.com>
2017-12-10 21:16:30 -0800

    added pi_interface skeleton

--- python/thermster/pi_interface.py
+++ python/thermster/pi_interface.py
@@ -1,2 +1,23 @@
 import logging
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
 
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+class PiInterface():
+    def __init__(self, name, temps, temp_lock, get_setpoint, setpoint_lock):
+        self.name = name
+        self.temps = temps
+        self.temp_lock = temp_lock
+        self.get_setpoint = get_setpoint
+        self.setpoint_lock = setpoint_lock
+        self.setpoint = self.update_setpoint()
+
+        self.DC = 23
+        self.disp = Adafruit_SSD1306.SSD1306_128_32(rst=None)
+
+    def update_setpoint(self):
+        with self.setpoint_lock:
+            self.setpoint = self.get_setpoint()
