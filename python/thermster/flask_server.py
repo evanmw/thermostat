8198dbfd17dfa4cd82878983c2ed18f4a22d9b5e
Evan Wilson <emwilson02@gmail.com>
2017-12-23 01:10:21 -0800

    added schedule display to website

--- python/thermster/flask_server.py
+++ python/thermster/flask_server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 import logging
+from thermostat_scheduler import ThermostatScheduler
 
 class WebServer():
     def __init__(self, name, data):
@@ -10,16 +11,25 @@ class WebServer():
         self.temps = data.temps
         self.get_setpoint = data.get_setpoint
         self.set_setpoint = data.set_setpoint
+
+        self.scheduler = ThermostatScheduler(data.sheshule,
+                                             data.shesh_lock,
+                                             data.SCHEDULE_FILE_PATH,
+                                             data.set_setpoint)
         
         # define routes
         self.app.add_url_rule('/', 'home', view_func=self.index)
         self.app.add_url_rule('/_set_temp', 'asdf', view_func=self.set_temp)
         self.app.add_url_rule('/_poll_data', 'asdfs', view_func=self.poll_data)
+        self.app.add_url_rule('/_get_schedule', 'ksks', view_func=self.get_schedule)
         
     def index(self):
         temp=round(self.temps['local'][-1][1], 1)
         setpoint_temp=round(self.get_setpoint()[0], 1)
-        return render_template('index.html', temp=temp, setpoint_temp=setpoint_temp)
+        return render_template('index.html',
+                               temp=temp,
+                               setpoint_temp=setpoint_temp,
+                               schedule_entries=self.scheduler.read_schedule())
 
     def set_temp(self):
         current_setpoint = self.get_setpoint()
@@ -36,7 +46,7 @@ class WebServer():
     
     def run(self):
         logging.warn("starting flask")
-        self.app.run(host='0.0.0.0', port=80, debug=True, use_reloader=False)
+        self.app.run(host='0.0.0.0', port=5678, debug=True, use_reloader=False)
         logging.warn("closing flask")
 
         
